dfbbfae18dadebdce9e57eab93986fac29b7cb0a
Anna Domracheva <annadomracheva@MacBook-Pro-Anna.local>
2024-06-21 19:27:32 +0300

    Добавлен делегат

---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Anna Domracheva on 11.06.2024.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion()
+}
