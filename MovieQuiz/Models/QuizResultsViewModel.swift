9c8bdf70883f7f7627038b6db71e63abca5dc814
Anna Domracheva <annadomracheva@MacBook-Pro-Anna.local>
2024-06-21 19:27:32 +0300

    Добавлен делегат

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Anna Domracheva on 10.06.2024.
+//
+
+import Foundation
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
